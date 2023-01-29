de64246962d6c03e232340ed7a13bdf6030df611
picksecure <78667364+picksecure@users.noreply.github.com>
2023-01-29 01:06:22 -0700

    fix

--- backend/server.js
+++ backend/server.js
@@ -85,8 +85,8 @@ app.use("/api", apiRoutes);
 
 const path = require("path");
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "/frontend/build")));
-    app.get("*", (req, res) => res.sendFile(path.resolve(__dirname, "/frontend", "build", "index.html")));
+    app.use(express.static(path.join(__dirname, "../frontend/build")));
+    app.get("*", (req, res) => res.sendFile(path.resolve(__dirname, "../frontend", "build", "index.html")));
 } else {
     app.get("/", (req, res) => {
         res.json({ message: "API running..." });
