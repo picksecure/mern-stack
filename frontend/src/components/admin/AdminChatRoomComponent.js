f34f5e1eea253a6fc70ef093616e648afa701d51
picksecure <78667364+picksecure@users.noreply.github.com>
2023-02-01 22:26:23 -0700

    test

--- frontend/src/components/admin/AdminChatRoomComponent.js
+++ frontend/src/components/admin/AdminChatRoomComponent.js
@@ -1,7 +1,7 @@
 import { Toast, Button, Form } from "react-bootstrap";
 import { Fragment, useState, useEffect } from "react";
 import { setMessageReceived } from "../../redux/actions/chatActions";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 const AdminChatRoomComponent = ({ chatRoom, roomIndex, socket, socketUser }) => {
 
@@ -47,51 +47,51 @@ const AdminChatRoomComponent = ({ chatRoom, roomIndex, socket, socketUser }) => 
   })
 
   return (
-    <>
-      <Toast
-        show={"toast" + roomIndex}
-        onClose={() => close(chatRoom[0])}
-        className="ms-4 mb-5"
-      >
-        <Toast.Header>
-          <strong className="me-auto">Chat with User</strong>
-        </Toast.Header>
-        <Toast.Body>
-          <div className={`cht-msg${socketUser}`} style={{ maxHeight: "500px", overflow: "auto" }}>
-            {chatRoom[1].map((msg, idx) => (
-              <Fragment key={idx}>
-                {msg.client && (
-                  <p
-                    key={idx}
-                    className="bg-primary p-3 ms-4 text-light rounded-pill"
-                  >
-                    <b>User wrote:</b> {msg.client}
-                  </p>
-                )}
-                {msg.admin && (
-                  <p key={idx}>
-                    <b>Admin wrote:</b> {msg.admin}
-                  </p>
-                )}
-              </Fragment>
-            ))}
-          </div>
+      <>
+              <Toast
+                  show={"toast" + roomIndex}
+                  onClose={() => close(chatRoom[0])}
+                  className="ms-4 mb-5"
+              >
+                  <Toast.Header>
+                      <strong className="me-auto">Chat with User</strong>
+                  </Toast.Header>
+                  <Toast.Body>
+                      <div className={`cht-msg${socketUser}`} style={{ maxHeight: "500px", overflow: "auto" }}>
+                          {chatRoom[1].map((msg, idx) => (
+                              <Fragment key={idx}>
+                                  {msg.client && (
+                                      <p
+                                          key={idx}
+                                          className="bg-primary p-3 ms-4 text-light rounded-pill"
+                                      >
+                                          <b>User wrote:</b> {msg.client}
+                                      </p>
+                                  )}
+                                  {msg.admin && (
+                                      <p key={idx}>
+                                          <b>Admin wrote:</b> {msg.admin}
+                                      </p>
+                                  )}
+                              </Fragment>
+                          ))}
+                      </div>
 
-          <Form>
-            <Form.Group
-              className="mb-3"
-              controlId={`adminChatMsg${roomIndex}`}
-            >
-              <Form.Label>Write a message</Form.Label>
-              <Form.Control onKeyUp={(e) => adminSubmitChatMsg(e, `adminChatMsg${roomIndex}`)} as="textarea" rows={2} />
-            </Form.Group>
-            <Button onClick={(e) => adminSubmitChatMsg(e, `adminChatMsg${roomIndex}`)} variant="success" type="submit">
-              Submit
-            </Button>
-          </Form>
-        </Toast.Body>
-      </Toast>
-    </>
+                      <Form>
+                          <Form.Group
+                              className="mb-3"
+                              controlId={`adminChatMsg${roomIndex}`}
+                          >
+                              <Form.Label>Write a message</Form.Label>
+                              <Form.Control onKeyUp={(e) => adminSubmitChatMsg(e, `adminChatMsg${roomIndex}`)} as="textarea" rows={2} />
+                          </Form.Group>
+                          <Button onClick={(e) => adminSubmitChatMsg(e, `adminChatMsg${roomIndex}`)} variant="success" type="submit">
+                              Submit
+                          </Button>
+                      </Form>
+                  </Toast.Body>
+              </Toast>
+             </>
   );
 };
 
