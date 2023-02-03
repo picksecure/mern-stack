caf1e8c4d3395e4e16dddaf80bf5ae90277fad8e
picksecure <78667364+picksecure@users.noreply.github.com>
2023-02-02 18:46:10 -0700

    gu

--- frontend/config-overrides.js
+++ frontend/config-overrides.js
@@ -6,10 +6,10 @@ const cspConfigPolicy = {
     'base-uri': "'self'",
     'object-src': "'none'",
     'img-src': "'self' data: *",
-    'script-src-elem': ["'self' 'unsafe-inline'"],
-    'script-src': ["'self' https://maps.googleapis.com/maps/api/js"],
+    'script-src-elem': ["'self' 'unsafe-inline' https://maps.googleapis.com/maps/api/js"],
+    'script-src': ["'self' https://maps.googleapis.com/maps/api/js 'unsafe-inline'"],
     'style-src': "'self' 'unsafe-inline'",
-    'style-src-elem': "'self' 'unsafe-inline' https://maps.googleapis.com/maps/api/js",
+    'style-src-elem': "'self' 'unsafe-inline'",
     'style-src-attr': "'self' 'unsafe-inline'"
 };
 
